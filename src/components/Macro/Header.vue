aef4151019e93befea08b94c7293ccbf78c209b6
AlexsandroD <adrdra88@gmail.com>
2022-01-11 13:28:03 +0100

    Logo

--- src/components/Macro/Header.vue
+++ src/components/Macro/Header.vue
@@ -1,78 +1,40 @@
 <template>
   <header>
-      <div class="info-bar">
-
-          <div class="item-container">
-            <div class="items1">
-                <div class="item">
-                    <i class="fas fa-clock "></i>
-                    <span>Open Hours: Mon - Sat - 9:00 - 18:00</span>
-                </div>
-            </div>
-
-            <div class="items2">
-                <div class="item phone">
-                    <i class="fas fa-phone-alt"></i>
-                    <span>+1(305) 1234-5678 </span>
-                </div>
-                <div class="item mail">
-                    <i class="fas fa-envelope"></i>
-                    <span>hello@example.com </span>
-                </div>
-                <div class="item icons">
-                    <i class="fab fa-facebook-f icon"></i>
-                    <i class="fab fa-twitter icon"></i>
-                    <i class="fab fa-twitter icon"></i>                    
-                </div>
-            </div>
-          </div>
-      </div>
-      <div class="nav-bar"></div>
+      <div class="filter"></div>
+      <InfoBar/>
+      <NavBar/>
   </header>
 </template>
 
 <script>
+import InfoBar from "../partials/InfoBar.vue";
+import NavBar from "../partials/NavBar.vue";
 export default {
     name:"Header",
+    components:{
+        InfoBar,
+        NavBar
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../../assets/style/global.scss";
-.info-bar {
-    width: 100%;
-    height: 40px;
-    background-color:$dark-blue;
-.item-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 0.90rem;
-    color: #fff;
-    height: 40px;
-    width:70%;
-    margin: auto;
-    .items1{
-        width: 50%;
-    }
-
-    .items2 {
-        display: flex;
-        justify-content: flex-end;
-        width: 50%;
-        .item{
-            margin: 0 10px;
-            & .icon{
-                margin: 0 15px
-            }
-
-            & span{
-                margin-left: 5px;
-            }
-        }
-    }
-}
-
+header{
+    position: relative;
+    height: 600px;
+    background-image: url("../../assets/img/bg-9.jpg");
+    background-repeat: no-repeat;
+    background-size:cover;
+    & .filter{
+        position:absolute;
+        background-color: rgba(0, 0, 0, 0.398);
+        top:0;
+        right:0;
+        bottom: 0;
+        left: 0;
+   }
+    
 }
 
 
---
+++ src/components/partials/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navigation-bar">
+      <div class="logo">
+          <span class="left">nex</span>
+          <span class="right">gen</span>
+      </div>
+      <div class="menu"></div>
+      <div class="button"></div>
+  </nav>
+</template>
+
+<script>
+export default {
+    name:"NavBar"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/global.scss";
+    nav.navigation-bar{
+        position: relative;
+        z-index: 10;
+        display: flex;
+        align-items: center;
+        width: 70%;
+        margin: auto;
+        height: 80px;
+        & .logo{
+            text-transform: uppercase;
+            font-weight: 700;
+            letter-spacing:2px;
+            color: white;
+            & .left{
+                border-radius: 15px 0px 0px 15px;
+                padding-left: 15px;
+                padding-top: 5px;
+                padding-bottom: 5px;
+                height: 80px;
+                background-color:#0384844a;
+                color:$blue-lagoon;
+            }
+        }
+    }
+ 
+</style>
